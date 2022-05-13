3b741bb253da463b8514b91a2a8401c3606c8722
galihif <galihindra2401@mail.ugm.ac.id>
2022-05-13 20:36:04 +0700

    refactor: rename MainActivity.kt to SplashActivity.kt

--- Artjuna-App/app/src/main/java/com/giftech/artjuna_app/ui/SplashActivity.kt
+++ Artjuna-App/app/src/main/java/com/giftech/artjuna_app/ui/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.giftech.artjuna_app
+package com.giftech.artjuna_app.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.giftech.artjuna_app.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
