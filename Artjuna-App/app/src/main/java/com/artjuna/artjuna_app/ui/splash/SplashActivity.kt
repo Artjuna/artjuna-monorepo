8ec788229736fe8c344f61c2cd767023c65ec18d
galihif <galihindra2401@mail.ugm.ac.id>
2022-05-15 14:26:04 +0700

    style: slicing activity_sign_up.xml

--- Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/splash/SplashActivity.kt
+++ Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/splash/SplashActivity.kt
@@ -1,10 +1,16 @@
 package com.artjuna.artjuna_app.ui.splash
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.artjuna.artjuna_app.databinding.ActivitySplashBinding
+import com.artjuna.artjuna_app.ui.auth.SignUpActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivitySplashBinding
@@ -14,6 +20,16 @@ class SplashActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         disableNightMode()
+        moveNextPage()
+    }
+
+    private fun moveNextPage() {
+        val delayTime = 2000L
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
+        }, delayTime)
     }
 
     private fun disableNightMode(){
