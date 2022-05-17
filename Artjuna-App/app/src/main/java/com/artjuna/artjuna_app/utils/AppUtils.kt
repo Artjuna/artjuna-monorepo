3d376575e1ec8808811bcd71bf0f22f5c6bc5fc3
galihif <galihindra2401@mail.ugm.ac.id>
2022-05-17 21:20:06 +0700

    utils: add showToast method

--- Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/utils/AppUtils.kt
+++ Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/utils/AppUtils.kt
@@ -1,8 +1,10 @@
 package com.artjuna.artjuna_app.utils
 
+import android.content.Context
 import android.text.Html
 import android.text.Spanned
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 
 object AppUtils{
@@ -18,6 +20,10 @@ object AppUtils{
 
     fun getLikedBy(liked:Int): Spanned {
         return Html.fromHtml("Liked by <b>$liked people</b>")
+    }
+
+    fun showToast(context:Context, message:String){
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
 
 }
