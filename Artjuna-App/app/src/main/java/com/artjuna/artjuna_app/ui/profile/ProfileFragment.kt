b1e6bbfa7986267023d2a3c80e36ed20436cc190
galihif <galihindra2401@mail.ugm.ac.id>
2022-05-17 14:46:24 +0700

    feat: slicing profile page

--- Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/profile/ProfileFragment.kt
+++ Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/profile/ProfileFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.artjuna.artjuna_app.databinding.FragmentProfileBinding
@@ -28,10 +27,7 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+
         return root
     }
 
