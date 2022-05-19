23907c190af06ae3a2165c46c9ab7419d64aba85
galihif <galihindra2401@mail.ugm.ac.id>
2022-05-19 22:11:10 +0700

    feat: add mystore activity

---
+++ Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/mystore/MyStoreActivity.kt
@@ -0,0 +1,12 @@
+package com.artjuna.artjuna_app.ui.mystore
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.artjuna.artjuna_app.R
+
+class MyStoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_store)
+    }
+}
