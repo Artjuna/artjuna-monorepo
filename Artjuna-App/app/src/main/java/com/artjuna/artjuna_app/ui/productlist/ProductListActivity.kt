d74fbfd44a73ec0ea2bc663b581f02abe350aff9
galihif <galihindra2401@mail.ugm.ac.id>
2022-05-20 15:08:22 +0700

    feat: show retrieved list from intent

--- Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/productlist/ProductListActivity.kt
+++ Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/productlist/ProductListActivity.kt
@@ -2,14 +2,44 @@ package com.artjuna.artjuna_app.ui.productlist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.artjuna.artjuna_app.core.data.source.model.Product
 import com.artjuna.artjuna_app.databinding.ActivityProductListBinding
+import com.artjuna.artjuna_app.ui.productlist.adapter.ProductAdapter
 
 class ProductListActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityProductListBinding
+    private val productAdapter = ProductAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProductListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setAdapter()
+        setData()
+        populateView()
+    }
+
+    private fun populateView() {
+        if(intent.extras != null){
+            val title = this.intent.extras?.getString(EXTRA_PAGE_TITLE)
+            binding.tvTitle.text = title
+        }
+    }
+
+    private fun setData() {
+        if(intent.extras != null){
+            val productList = this.intent.extras?.getParcelableArrayList<Product>(EXTRA_PRODUCT_LIST)
+            productAdapter.submitList(productList)
+        }
+    }
+
+    private fun setAdapter() {
+        binding.rvProduct.adapter = productAdapter
+    }
+
+    companion object{
+        const val EXTRA_PAGE_TITLE = "EXTRA_PAGE_TITLE"
+        const val EXTRA_PRODUCT_LIST = "EXTRA_PRODUCT_LIST"
     }
 }
---
+++ Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/productlist/adapter/ProductAdapter.kt
@@ -0,0 +1,59 @@
+package com.artjuna.artjuna_app.ui.productlist.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.artjuna.artjuna_app.core.data.source.model.Product
+import com.artjuna.artjuna_app.databinding.ItemProductBinding
+import com.artjuna.artjuna_app.ui.detailproduct.DetailProductActivity
+import com.artjuna.artjuna_app.utils.AppUtils.loadImage
+
+class ProductAdapter:ListAdapter<Product, ProductAdapter.ProductViewHolder>(DIFF_CALLBACK) {
+
+    class ProductViewHolder(private val binding: ItemProductBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(product: Product){
+            with(binding){
+                ivImage.loadImage(product.image)
+                tvName.text = product.name
+                tvPrice.text = "Rp ${product.price}"
+                tvCity.text = product.storeCity
+
+                itemView.setOnClickListener {
+                    val intent = Intent(itemView.context, DetailProductActivity::class.java)
+                    itemView.context.startActivity(intent)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val binding =
+            ItemProductBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ProductViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        val product = getItem(position)
+        if(product!=null){
+            holder.bind(product)
+        }
+    }
+
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Product>() {
+            override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+                return oldItem.id == newItem.id
+            }
+        }
+    }
+
+
+}
