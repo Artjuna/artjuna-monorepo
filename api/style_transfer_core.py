41cbe8ad44a6f417517b1b15b5ee351779b6fd6a
Aditya Wardianto <83759122+ditw11mhs@users.noreply.github.com>
2022-05-23 14:42:06 +0700

    Added image preprocessing

--- api/style_transfer_core.py
+++ api/style_transfer_core.py
@@ -9,3 +9,14 @@ class StyleTransferModel:
         # Turn raw bytes to image
         image = tf.image.decode_image(image_b64_raw, channels=3)
         return image
+
+    def image_preprocessing(self,image):
+        image = tf.cast(image,tf.float32)
+        image = image/255.0
+        image = tf.image.resize(image,(224,224))
+        return image
+
+    def image_converter(self, image_b64):
+        image = self.image_decoder(image_b64)
+        image = self.image_preprocessing(image)
+        return image
