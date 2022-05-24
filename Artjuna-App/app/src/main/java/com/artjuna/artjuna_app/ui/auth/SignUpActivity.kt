47b9a19841f7a8e0f1e2306de49fae8f1bfe02c3
dheniyaaa <dhenipratiwi5@mail.ugm.ac.id>
2022-05-24 22:24:35 +0700

    logic signUp activity

--- Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/auth/SignUpActivity.kt
+++ Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/auth/SignUpActivity.kt
@@ -2,16 +2,34 @@ package com.artjuna.artjuna_app.ui.auth
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.artjuna.artjuna_app.databinding.ActivitySignUpBinding
 import com.artjuna.artjuna_app.ui.navigation.NavigationActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding:ActivitySignUpBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+
+    companion object{
+        var TAG = SignUpActivity::class.java.simpleName
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        db = Firebase.firestore
 
         binding.btnSignIn.setOnClickListener {
             startActivity(Intent(this, SignInActivity::class.java))
@@ -22,4 +40,68 @@ class SignUpActivity : AppCompatActivity() {
             finish()
         }
     }
+
+    private fun signUp(){
+        val fullName = binding.etFullName.text.toString().trim()
+        val username = binding.etUsername.text.toString().trim()
+        val email = binding.etEmail.text.toString().trim()
+        val password = binding.etPassword.text.toString().trim()
+
+        if(fullName.isEmpty()){
+            binding.etFullName.error = "Please enter this field"
+            return
+        }
+        if(username.isEmpty()){
+            binding.etUsername.error = "Please enter this field"
+            return
+        }
+        if(email.isEmpty()){
+            binding.etEmail.error = "Please enter this field"
+            return
+        }
+        if(password.isEmpty()){
+            binding.etPassword.error = "Please enter this field"
+            return
+        }
+
+        binding.btnSignUp.visibility = View.INVISIBLE
+
+        db.collection("users").get()
+            .addOnSuccessListener { result ->
+                for (document in result){
+                    Log.d(TAG, "addOnSuccessListener ${document.data["username"]}")
+                    if (document.data["username"] == username){
+                        Log.d(TAG, "addOnSuccessListener ${document.data["username"]}")
+                        binding.etUsername.error = "Username already exist"
+                        Toast.makeText(this, "Username already exist", Toast.LENGTH_SHORT).show()
+                        binding.btnSignUp.visibility =View.VISIBLE
+                        return@addOnSuccessListener
+                    }
+                }
+                userRegister(fullName, username, email, password)
+            }
+            .addOnFailureListener { e ->
+                binding.btnSignUp.visibility = View.VISIBLE
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            }
+
+    }
+
+    private fun userRegister(fullName: String, username: String, email: String, password: String) {
+
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
