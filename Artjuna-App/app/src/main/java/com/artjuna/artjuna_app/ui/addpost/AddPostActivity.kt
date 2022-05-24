434bd58d640bb7f38c476aed1e06892dce6e1875
galihif <galihindra2401@mail.ugm.ac.id>
2022-05-24 09:07:59 +0700

    feat: add image from gallery in add product and post

--- Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/addpost/AddPostActivity.kt
+++ Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/addpost/AddPostActivity.kt
@@ -1,12 +1,43 @@
 package com.artjuna.artjuna_app.ui.addpost
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.artjuna.artjuna_app.R
+import com.artjuna.artjuna_app.databinding.ActivityAddPostBinding
+import com.artjuna.artjuna_app.utils.AppUtils
+import java.io.File
 
 class AddPostActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityAddPostBinding
+    private var photoFile: File? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_post)
+        binding = ActivityAddPostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun openGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = AppUtils.uriToFile(selectedImg, this)
+
+            photoFile = myFile
+
+            binding.ivImage.setImageURI(selectedImg)
+        }
     }
 }
