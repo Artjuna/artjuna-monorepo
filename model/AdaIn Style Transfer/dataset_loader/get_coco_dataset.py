4f527b454b1aa21ae1a718a8814094d359eaa46d
Adi Kelvianto <92104520+adikelvianto@users.noreply.github.com>
2022-05-25 23:37:16 +0700

    Update get_coco_dataset.py

--- model/AdaIn Style Transfer/dataset_loader/get_coco_dataset.py
+++ model/AdaIn Style Transfer/dataset_loader/get_coco_dataset.py
@@ -1,5 +1,8 @@
 import fiftyone as fo
 import fiftyone.zoo as foz
 
+''' We wont use COCO as main data set because it's a very big dataset,
+then only the validation set will be used to add train, val and test dataset on main VOC Pascal dataset'''
+
 # Change split method between 'train', 'test' or 'validation'
 dataset = foz.load_zoo_dataset("coco-2017", split="validation")
