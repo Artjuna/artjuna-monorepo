f5878518651da173c5992ee21a3364b898f307a7
galihif <galihindra2401@mail.ugm.ac.id>
2022-05-30 21:22:14 +0700

    feat: success upload product to api

--- Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/addproduct/AddProductViewModel.kt
+++ Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/addproduct/AddProductViewModel.kt
@@ -2,10 +2,10 @@ package com.artjuna.artjuna_app.ui.addproduct
 
 import androidx.lifecycle.ViewModel
 import com.artjuna.artjuna_app.core.data.repositories.MainRepository
-import com.artjuna.artjuna_app.core.data.source.model.Post
+import com.artjuna.artjuna_app.core.data.source.model.Product
 
 class AddProductViewModel(private val mainRepository: MainRepository) : ViewModel() {
 
-    fun uploadPost(post:Post) = mainRepository.uploadPost(post)
+    fun uploadProduct(product: Product) = mainRepository.uploadProduct(product)
 
 }
