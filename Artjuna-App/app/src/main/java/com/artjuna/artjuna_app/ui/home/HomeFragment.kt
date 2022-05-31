78e7abe066281497bd00fc4363b1e9507af229dd
galihif <galihindra2401@mail.ugm.ac.id>
2022-05-31 20:15:42 +0700

    feat: get all product from api

--- Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/home/HomeFragment.kt
+++ Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/home/HomeFragment.kt
@@ -10,7 +10,6 @@ import com.artjuna.artjuna_app.core.data.source.model.Product
 import com.artjuna.artjuna_app.core.data.source.remote.network.Result
 import com.artjuna.artjuna_app.databinding.FragmentHomeBinding
 import com.artjuna.artjuna_app.ui.home.adapter.RecomAdapter
-import com.artjuna.artjuna_app.ui.productlist.ProductListActivity
 import com.artjuna.artjuna_app.ui.search.SearchActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -46,12 +45,12 @@ class HomeFragment : Fragment() {
         with(binding){
             header
             header.btnSearch.setOnClickListener { startActivity(Intent(requireContext(), SearchActivity::class.java)) }
-            recom.btnSeeAll.setOnClickListener {
-                val intent = Intent(requireContext(), ProductListActivity::class.java)
-                intent.putParcelableArrayListExtra(ProductListActivity.EXTRA_PRODUCT_LIST, recomList)
-                intent.putExtra(ProductListActivity.EXTRA_PAGE_TITLE, "Recommendation")
-                startActivity(intent)
-            }
+//            recom.btnSeeAll.setOnClickListener {
+//                val intent = Intent(requireContext(), ProductListActivity::class.java)
+//                intent.putParcelableArrayListExtra(ProductListActivity.EXTRA_PRODUCT_LIST, recomList)
+//                intent.putExtra(ProductListActivity.EXTRA_PAGE_TITLE, "Recommendation")
+//                startActivity(intent)
+//            }
         }
     }
 
