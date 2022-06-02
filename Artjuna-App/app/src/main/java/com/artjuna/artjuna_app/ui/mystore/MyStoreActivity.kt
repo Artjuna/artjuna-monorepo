a06691a2feded80c26f210393c490fd2488c11ef
galihif <galihindra2401@mail.ugm.ac.id>
2022-06-02 17:35:01 +0700

    feat: populate user data to my store page

--- Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/mystore/MyStoreActivity.kt
+++ Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/mystore/MyStoreActivity.kt
@@ -2,6 +2,7 @@ package com.artjuna.artjuna_app.ui.mystore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.artjuna.artjuna_app.core.data.source.model.User
 import com.artjuna.artjuna_app.databinding.ActivityMyStoreBinding
 import com.artjuna.artjuna_app.ui.mystore.adapter.MyStorePagerAdapter
 import com.google.android.material.tabs.TabLayoutMediator
@@ -14,8 +15,27 @@ class MyStoreActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMyStoreBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        getUserData()
         showTabLayout()
+        setButtonClick()
+    }
+
+    private fun setButtonClick() {
+        binding.header.btnBack.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun getUserData() {
+        val user = intent.extras?.getParcelable<User>(EXTRA_USER)
+        populateView(user!!)
+    }
+
+    private fun populateView(user: User) {
+        with(binding){
+            header.tvName.text = user.fullName
+            header.tvCityFollowers.text = "Kota ${user.city} | ${user.followers} Followers"
+        }
     }
 
     private fun showTabLayout() {
@@ -33,5 +53,6 @@ class MyStoreActivity : AppCompatActivity() {
             "Product",
             "Post"
         )
+        const val EXTRA_USER = "EXTRA_USER"
     }
 }
