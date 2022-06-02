e248d912a2c2ec0dc2be64ef6710fe5f39ef2174
Aditya Wardianto <83759122+ditw11mhs@users.noreply.github.com>
2022-06-02 15:05:23 +0700

    Added multifunction api starter

--- api/app.py
+++ api/app.py
@@ -1,11 +1,10 @@
 import os
 import sys
 
-
 arg = sys.argv
-if len(arg)==1:
+if len(arg) == 1:
     os.system("uvicorn ml_api.style_transfer_api:app")
-elif arg[1] in ["-r",'--reload']:
+elif arg[1] in ["-r", "--reload"]:
     os.system("uvicorn ml_api.style_transfer_api:app --reload")
 else:
     print("Invalid argument")
