9b64d66cc4789d377ea231dcc486f38840f5e387
Aditya Wardianto <83759122+ditw11mhs@users.noreply.github.com>
2022-06-02 15:05:45 +0700

    Reformat file

--- api/ml_api/style_transfer_core.py
+++ api/ml_api/style_transfer_core.py
@@ -1,13 +1,12 @@
 import os
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import base64
 
 import tensorflow as tf
 
 
 class StyleTransferModel:
-    
     def image_decoder(self, image_b64):
         # Turn base64 byte string to raw bytes
         image_b64_raw = base64.b64decode(image_b64)
@@ -15,13 +14,13 @@ class StyleTransferModel:
         image = tf.image.decode_image(image_b64_raw, channels=3)
         return image
 
-    def image_preprocessing(self,image):
+    def image_preprocessing(self, image):
         # Casting image to float32
-        image = tf.cast(image,tf.float32)
+        image = tf.cast(image, tf.float32)
         # Normalizing image to [0,1]
-        image = image/255.0
+        image = image / 255.0
         # Resizing image to 224x224
-        image = tf.image.resize(image,(224,224))
+        image = tf.image.resize(image, (224, 224))
         return image
 
     def image_converter(self, image_b64):
@@ -30,5 +29,3 @@ class StyleTransferModel:
         return image
 
     # def load_model(self,model_path):
-        
-    
