4a8dd75d284bcc6e582a86582248e299694a928a
Aditya Wardianto <83759122+ditw11mhs@users.noreply.github.com>
2022-06-02 15:05:45 +0700

    Reformat file

--- api/ml_api/style_transfer_api.py
+++ api/ml_api/style_transfer_api.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI, UploadFile
 from pydantic import BaseModel
-from style_transfer_core import StyleTransferModel
+
+from .style_transfer_core import StyleTransferModel
 
 app = FastAPI()
 inference_model = StyleTransferModel()
 
+
 class Image64(BaseModel):
-    image_string:str
+    image_string: str
+
 
 @app.get("/")
 async def root():
-    return {"root":"Success"}
+    return {"root": "Success"}
+
 
 @app.post("/inference")
 async def inference(image_b64: Image64):
     image = inference_model.image_converter(image_b64.image_string)
     print(image.numpy().tolist())
-    return {"result": 200 }
+    return {"result": 200}
