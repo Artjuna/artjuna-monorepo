3d31a01fb738a2a1f260812025d9f6ee76b5c1d4
nathanlau3 <nathanlau3@gmail.com>
2022-06-03 16:09:07 +0700

    Add update post, add Transaction, fixing image name

--- api/routes/PostRoute.js
+++ api/routes/PostRoute.js
@@ -1,7 +1,30 @@
 const PostController = require('../controller/PostController');
 const express = require('express');
+const path = require('path');
+const multer = require('multer');
+
 //router
 const router = require('express').Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'PostImages')
+    },
+    filename: (req, file, cb) => {
+        cb(null,   Date.now() + '-' + file.originalname )
+    }
+});
+const upload = multer({storage: storage,
+    fileFilter: (req, file, cb) => {
+        const fileTypes = /jpeg|jpg|png|gif/
+        const mimeType = fileTypes.test(file.mimetype)  ;
+        const extname = fileTypes.test(path.extname(file.originalname));
+        if(mimeType && extname) {
+            return cb(null, true)
+        }
+        cb('Give proper files formate to upload')
+    }
+});
 
 //use routers
 router.use(express.json({ limit: '50mb' }));
@@ -9,8 +32,9 @@ router.use(express.urlencoded({ limit: '50mb', extended: true }));
 //router
 
 //use routers
-router.post('/addPost', PostController.addPost);
+router.post('/addPost', upload.single('Image'), PostController.addPost);
 router.get('/getPost', PostController.getPost);
+router.put('/updatePost', upload.single('Image'), PostController.updatePost);
 
 module.exports = router;
 
