5549432fa07b524af3fcb885b67338921825c452
galihif <galihindra2401@mail.ugm.ac.id>
2022-06-05 21:14:14 +0700

    refactor: add isLiked prop

--- Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/core/data/source/model/Post.kt
+++ Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/core/data/source/model/Post.kt
@@ -1,13 +1,27 @@
 package com.artjuna.artjuna_app.core.data.source.model
 
+import com.artjuna.artjuna_app.core.data.source.local.entity.PostEntity
+
 data class Post(
     var id:String="",
     var userName:String="",
-    var userImage:String="",
     var userId:String="",
     var image:String="",
     var productName:String="",
-    var productId:String="",
     var like:Int=0,
-    var caption:String=""
+    var caption:String="",
+    var isLiked:Boolean = false
 )
+
+fun Post.toPostEntity():PostEntity{
+    return PostEntity(
+        id=id,
+        userName=userName,
+        userId=userId,
+        image=image,
+        productName=productName,
+        like=like,
+        caption=caption,
+        isLiked=isLiked
+    )
+}
