14f720e12b9db4ffc9f04d770af48180e5088ef2
galihif <galihindra2401@mail.ugm.ac.id>
2022-06-05 12:24:05 +0700

    feat: add and remove product to cart

--- Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/detailproduct/DetailProductActivity.kt
+++ Artjuna-App/app/src/main/java/com/artjuna/artjuna_app/ui/detailproduct/DetailProductActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.artjuna.artjuna_app.R
 import com.artjuna.artjuna_app.core.data.source.model.Product
 import com.artjuna.artjuna_app.databinding.ActivityDetailProductBinding
 import com.artjuna.artjuna_app.ui.checkout.CheckoutActivity
 import com.artjuna.artjuna_app.ui.customize.CustomizeActivity
 import com.artjuna.artjuna_app.ui.store.StoreActivity
+import com.artjuna.artjuna_app.utils.AppUtils
 import com.artjuna.artjuna_app.utils.AppUtils.loadImage
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -16,6 +18,7 @@ class DetailProductActivity : AppCompatActivity() {
     private lateinit var binding:ActivityDetailProductBinding
     private val viewModel:DetailViewModel by viewModel()
     private var product = Product()
+    private var isProductInCart = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailProductBinding.inflate(layoutInflater)
@@ -50,7 +53,29 @@ class DetailProductActivity : AppCompatActivity() {
                 intent.putExtra(CustomizeActivity.EXTRA_PRODUCT, product)
                 startActivity(intent)
             }
+
+            bottomBar.btnCart.setOnClickListener {
+                if(isProductInCart){
+                    deleteProductFromCart()
+                }else{
+                    insertProductToCart()
+                }
+            }
         }
+    }
+
+    private fun insertProductToCart() {
+        viewModel.insertProductToCart(product)
+        isProductInCart = true
+        checkIfProductInCartandSetButton()
+        AppUtils.showToast(this, "Product Added to Cart")
+    }
+
+    private fun deleteProductFromCart() {
+        viewModel.deleteProductFromCartById(product.id)
+        isProductInCart = false
+        checkIfProductInCartandSetButton()
+        AppUtils.showToast(this, "Product Removed from Cart")
     }
 
     private fun getProductData() {
@@ -59,6 +84,24 @@ class DetailProductActivity : AppCompatActivity() {
             product = extras.getParcelable<Product>(EXTRA_PRODUCT)!!
             populateView()
             addHasSen()
+            checkIfProductInCartandSetButton()
+        }
+    }
+
+    private fun checkIfProductInCartandSetButton() {
+        viewModel.checkIfProductInCart(product.id).observe(this){
+            isProductInCart = it
+            setButtonCartIcon()
+        }
+    }
+
+    private fun setButtonCartIcon() {
+        with(binding){
+            if(isProductInCart){
+                bottomBar.btnCart.setImageResource(R.drawable.ic_cart_white)
+            }else{
+                bottomBar.btnCart.setImageResource(R.drawable.ic_cart_white_outline)
+            }
         }
     }
 
